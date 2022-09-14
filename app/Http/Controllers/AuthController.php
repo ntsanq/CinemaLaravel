36e623059f8fdb25668039fc80ce9f7850224d0f
Nguyen Thanh Sang <ntsanq@gmail.com>
2022-09-14 13:47:35 +0700

    feat: sign in + log in

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -13,31 +14,33 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        return $request->all();
+        $user = User::where('email', $request->email)->first();
+        if (!$user || Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'user not found or password is wrong'
+            ]);
+        }
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
     }
 
     /**
      * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
     public function register(Request $request)
     {
-        $rules = [
-            'email' => 'email|required|unique:users',
-            'password' => 'required',
-            'name' => 'required',
-            'birthday' => 'required',
-        ];
 
-        $validator = Validator::make($request->all(), $rules);
-
+        $validator = $this->checkValidation($request);
         if ($validator->fails()) {
             return response()->json([
-                'messages' => $validator->messages(),
+                'errors' => $validator->messages(),
             ], 400);
         }
-
-        User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
@@ -45,7 +48,36 @@ class AuthController extends Controller
         ]);
 
         return response()->json([
-            "messages"=>User::all()
-        ],201);
+            "status" => 'success',
+            "data" => [
+                'id' => $user->id,
+                'email' => $user->email
+            ]
+        ], 201);
+    }
+
+    private function checkValidation($request)
+    {
+        $rules = [
+            'email' => 'email|required|unique:users',
+            'password' => [
+                'required',
+                'string',
+                'min:10',
+                'regex:((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,50})'
+            ],
+            'name' => 'required',
+            'birthday' => [
+                'required',
+                'date',
+                'before:01/01/2020 00:00'
+            ],
+        ];
+        $messages = [
+            'password.regex' => 'Password must contain at least one number and both uppercase and lowercase letters.',
+            'birthday.before' => 'Your age have not permission to use this website.'
+        ];
+
+        return Validator::make($request->all(), $rules, $messages);
     }
 }
