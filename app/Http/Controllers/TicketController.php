0d048e90d618fe07eac09416e95e257506c54d5f
Sang Nguyen <ntsanq@gmail.com>
2022-12-15 10:55:06 +0700

    wip: date pick

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Film;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    public function date(Request $request)
+    {
+        $film = Film::where('films.id', $request->filmId)
+            ->join('images','images.id', 'films.image_id')
+            ->select([
+                'films.*',
+                'images.path',
+            ])
+            ->get()->first();
+
+        return view('ticket.datePick', [
+            'film' => $film
+        ]);
+    }
+
+    public function dateList(Request $request)
+    {
+        
+    }
+
+    public function seat()
+    {
+        return view('ticket.seatPick');
+    }
+
+}
--- resources/views/ticket/datePick.blade.php
+++ resources/views/ticket/datePick.blade.php
@@ -1,11 +1,64 @@
 @include('layouts/head')
-@yield('')
 
-<form class="uk-form">
-    <input type="text" data-uk-datepicker="{format:'DD.MM.YYYY'}">
-</form>
+<div class="film-info">
+    <div>
+        {{ $film->id }}
+    </div>
+    <div>
+        {{ $film->name }}
+    </div>
+    <div>
+        {{ $film->path }}
+    </div>
+</div>
+
+<div>
+    <h2 style="text-align: center; padding-top: 5rem;">Which day you want to watch?</h2>
+    <form class="uk-panel uk-panel-box uk-form" style="padding: 0 20rem;" id="date-form">
+        <input type="hidden" value="{{ app('request')->input('filmId') }}" name="filmId" id="filmId">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="uk-form-row">
+            <input class="uk-width-1-1 uk-form-large" type="date" name="date" id="date">
+        </div>
+        <div class="uk-form-row">
+            <input class="uk-width-1-1 uk-button uk-button-primary uk-button-large" type="submit" value="Choose">
+        </div>
+    </form>
+
+    <span id="response"></span>
+
+</div>
+
 
 <script>
+    const form = document.querySelector('#date-form');
+    const date = document.querySelector('#date');
 
-    var datepicker = UIkit.datepicker(element, { "{weekstart:0, format:'DD.MM.YYYY'}" });
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        try {
+            getSeats(date.value);
+
+        } catch (e) {
+
+        }
+    });
+
+    function getSeats(date) {
+        return fetch("http://localhost:8000/api/getSeatsAvailable", {
+            method: 'post',
+            body: {
+                date: date
+            }
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            });
+    }
 </script>
