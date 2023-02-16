7362a36b3f49a604654348426a23ece0aeae8631
Sang Nguyen <ntsanq@gmail.com>
2023-02-16 11:55:59 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
     server: {
+
+        https: true,
         host: 'cinema.sangnguyen.me'
     },
 
     plugins: [
+        basicSsl(),
         laravel({
             input: [
                 'resources/js/app.jsx',
