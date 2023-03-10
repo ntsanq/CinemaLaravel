506337acc9c234094a1e8cd21765a9aba58cabc3
Sang Nguyen <ntsanq@gmail.com>
2023-03-10 16:45:36 +0700

    [BE] feat: stripe payment (2)

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\TicketStatus;
 use App\Models\Film;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class TicketController extends Controller
 {
@@ -28,6 +31,37 @@ class TicketController extends Controller
     public function showReceipt(Request $request)
     {
         $jsonString = json_decode($request->input('tickets'));
+
         dd($jsonString);
+    }
+
+
+    public function success(Request $request)
+    {
+        $sessionId = $request->input('sessionId');
+
+        $tickets = Ticket::query()->where('session_id', $sessionId)->get()->toArray();
+
+        if (empty($tickets)){
+            throw new NotFoundHttpException;
+        }
+
+        $this->ticketsPaid($sessionId);
+
+
+        return view('checkout.success', [
+            'tickets' => $tickets
+        ]);
+    }
+
+    public function cancel()
+    {
+
+    }
+
+    private function ticketsPaid($sessionId)
+    {
+        return Ticket::query()->where('session_id', $sessionId)
+            ->update(['status' => TicketStatus::Paid]);
     }
 }
