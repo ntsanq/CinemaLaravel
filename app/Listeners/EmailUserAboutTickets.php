d75125886ee27a25810c679ea62ad470655dd0c8
Nguyen Thanh Sang <ntsanq@gmail.com>
2023-04-19 18:50:02 +0700

    feat: implement queue (#17)

--- app/Listeners/EmailUserAboutTickets.php
+++ app/Listeners/EmailUserAboutTickets.php
@@ -4,11 +4,12 @@ namespace App\Listeners;
 
 use App\Events\SuccessTicketBooked;
 use App\Mail\SuccessTicketMessage;
+use App\Models\Ticket;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class EmailUserAboutTickets
+class EmailUserAboutTickets implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -23,11 +24,39 @@ class EmailUserAboutTickets
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param object $event
      * @return void
      */
     public function handle(SuccessTicketBooked $event)
     {
-        Mail::to($event->email)->send(new SuccessTicketMessage($event->sessionId));
+        $ticketIds = Ticket::query()->where('session_id', $event->sessionId)->get('id')->toArray();
+        $ticketData = [];
+        foreach ($ticketIds as $ticketId) {
+            $ticket = Ticket::query()
+                ->join('schedules', 'schedules.id', 'tickets.schedule_id')
+                ->join('films', 'films.id', 'schedules.film_id')
+                ->join('seats', 'seats.id', 'tickets.seat_id')
+                ->join('seat_categories', 'seat_categories.id', 'seats.seat_category_id')
+                ->join('users', 'users.id', 'tickets.user_id')
+                ->select([
+                    'tickets.id',
+                    'tickets.id',
+                    'schedules.start as start_time',
+                    'schedules.end as end_time',
+                    'films.name as film_name',
+                    'seats.name as seat_name',
+                    'users.name as user_name',
+                    'seat_categories.name as seat_type',
+                ])
+                ->where('tickets.id', $ticketId)->first()->toArray();
+
+            $ticketData[] = $ticket;
+        }
+
+        foreach ($ticketData as &$ticketItem) {
+            $ticketItem['start_time'] = date("H:i", strtotime($ticketItem['start_time']));
+        }
+
+        Mail::to($event->email)->send(new SuccessTicketMessage($ticketData));
     }
 }
--- resources/views/mail/success-message.blade.php
+++ resources/views/mail/success-message.blade.php
@@ -1,3 +1,24 @@
 <h1>
-    You're ticket has been booked, Your session ID is {{ $sessionId }}
+    You successfully booked ticket from SAN Cinema:
+
+    <br>
+    @foreach($ticketsData as $ticket)
+        "id": {{ $ticket['id'] }},
+        <br>
+        "start_time": {{ $ticket['start_time'] }},
+        <br>
+        "end_time": {{ $ticket['end_time'] }},
+        <br>
+        "film_name": {{ $ticket['film_name'] }},
+        <br>
+        "seat_name": {{ $ticket['seat_name'] }},
+        <br>
+        "user_name": {{ $ticket['user_name'] }},
+        <br>
+        "seat_type": {{ $ticket['seat_type'] }}
+        <br>
+        ---------------------
+        <br>
+    @endforeach
+
 </h1>
