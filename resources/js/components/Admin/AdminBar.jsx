ba76b37ed26a20efcb0a5e8f2bf0cd67184329f2
Sang Nguyen <ntsanq@gmail.com>
2023-04-25 16:04:51 +0700

    feat: light/dark theme options

--- resources/js/components/Admin/AdminBar.jsx
+++ resources/js/components/Admin/AdminBar.jsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
-import {AppBar} from 'react-admin';
+import {AppBar, ToggleThemeButton, defaultTheme} from 'react-admin';
 import Box from '@mui/material/Box';
 import {Button} from "antd";
+import {createTheme} from "@mui/material";
 
 export default function AdminBar() {
     const handleLogout = () => {
         window.location.href = `/admin/logout`;
     }
 
+    const darkTheme = createTheme({
+        palette: { mode: 'dark' },
+    });
+
     return (
         <AppBar color="primary">
             <Box flex="1"/>
             <Box flex="1"/>
+            <ToggleThemeButton lightTheme={defaultTheme} darkTheme={darkTheme} />
             <Button type="text" style={{color: "white"}} onClick={handleLogout}>Log out</Button>
         </AppBar>
     );
